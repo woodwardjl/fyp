c37a83ab93237a82b54f90a2563ff172bd078a6f
^-^ <woodwardjosephluke@gmail.com>
2018-03-01 17:30:14 +0000

    aesthetic changes.

--- Parser.hs
+++ Parser.hs
@@ -19,7 +19,7 @@ addsubexpr toks = let (multdivtree, toks') = multdivexpr toks
                     case peek toks' of
                       (T.TokenOperator op) | elem op [T.Plus, T.Minus]
                                   -> let (addsubtree, toks'') = expr $ toktail toks'
-                             in (T.AddSubNode op multdivtree addsubtree, toks'')
+                                     in (T.AddSubNode op multdivtree addsubtree, toks'')
                       _           -> (multdivtree, toks')
 
 multdivexpr :: [T.Token] -> (T.Tree, [T.Token])
@@ -28,7 +28,7 @@ multdivexpr toks = let (primarytree, toks') = conditionalexpr toks
                      case peek toks' of
                        T.TokenOperator op | op `elem` [T.Mult, T.Div]
                                   -> let (primarytree', toks'') = multdivexpr (toktail toks')
-                              in (T.MultDivNode op primarytree primarytree', toks'')
+                                     in (T.MultDivNode op primarytree primarytree', toks'')
                        _          -> (primarytree, toks')
 
 conditionalexpr :: [T.Token] -> (T.Tree, [T.Token])
@@ -37,19 +37,18 @@ conditionalexpr toks = let (conditionaltree, toks') = exprprimary toks
                          case peek toks' of
                            T.TokenComparison op
                                   -> let (primarytree, toks'') = multdivexpr (toktail toks') 
-                                 in (T.ComparisonNode op primarytree conditionaltree, toks'')
+                                     in (T.ComparisonNode op primarytree conditionaltree, toks'')
                            _      -> (conditionaltree, toks')
 
 exprprimary :: [T.Token] -> (T.Tree, [T.Token])
 exprprimary toks = case peek toks of
-                     T.TokenInt x -> (T.LiteralNode x, toktail toks)
-                     T.TokenBraceL
-                                  -> let (expTree, toks') = expr (toktail toks)
-                            in (expTree, toktail toks')
+                     T.TokenInt x  -> (T.LiteralNode x, toktail toks)
+                     T.TokenBraceL -> let (expTree, toks') = expr (toktail toks)
+                                      in (expTree, toktail toks')
                      T.TokenConditional o
-                                  -> let (termTree, toks') = multdivexpr (toktail toks)
-                            in (T.ConditionalNode o termTree, toks')         
-                     _            -> error $ "Parse error on token: " ++ show (peek toks)
+                                   -> let (termTree, toks') = multdivexpr (toktail toks)
+                                      in (T.ConditionalNode o termTree, toks')         
+                     _             -> error $ "Parse error on token: " ++ show (peek toks)
 
 parse' :: [[T.Token]] -> [T.Tree]
 parse' = \xs -> [parse x | x <- xs]
